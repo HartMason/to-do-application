ec37d592cfcece4e350bd2ec0afe300e82cbf985
HartMason <hartmason85@gmail.com>
2022-07-20 12:34:16 -0500

    trying to implement a delete button

--- src/App.js
+++ src/App.js
@@ -1,38 +1,37 @@
 import "./App.css";
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { render } from "@testing-library/react";
 
-class App extends Component { 
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       isClicked: false,
       todos: [],
-      text: ''
+      text: "",
     };
   }
-    
 
-  onChange = e => {
+  onChange = (e) => {
     this.setState({
-      text: e.target.value
-    })
-  }
+      text: e.target.value,
+    });
+  };
 
   onClickHandler = () => {
-    console.log(this.state.isClicked)
-    this.setState({isClicked: !this.state.isClicked})
-  }
+    console.log(this.state.todos);
+    this.setState({ text: this.state.todos }); //Not sure if this is correct
+  };
 
+  render() {
+    return (
+      <div>
+        <h1>To Do's</h1>
+        <input value={this.state.text} onChange={this.onChange} />
 
-  render () {
-  return ( 
-    <div>
-    <h1>To Do's</h1>
-    <input value={this.state.text} onChange={this.onChange}/>
-    <button onClick={this.onClickHandler}>Button</button>
-    </div>
-    )  
+        <button onClick={this.onClickHandler}>Button</button>
+      </div>
+    );
   }
 }
 
