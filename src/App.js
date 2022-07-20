b9766883b38779ebd2689a0ba814e1e40d62b3e9
HartMason <hartmason85@gmail.com>
2022-07-19 20:39:39 -0500

    onClickHandler is showing true and false in the console

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import "./App.css";
+import React, {Component} from "react";
+import { render } from "@testing-library/react";
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component { 
+  constructor(props) {
+    super(props);
+    this.state = {
+      isClicked: false,
+      todos: [],
+      text: ''
+    };
+  }
+    
+
+  onChange = e => {
+    this.setState({
+      text: e.target.value
+    })
+  }
+
+  onClickHandler = () => {
+    console.log(this.state.isClicked)
+    this.setState({isClicked: !this.state.isClicked})
+  }
+
+
+  render () {
+  return ( 
+    <div>
+    <h1>To Do's</h1>
+    <input value={this.state.text} onChange={this.onChange}/>
+    <button onClick={this.onClickHandler}>Button</button>
+    </div>
+    )  
+  }
 }
 
 export default App;
