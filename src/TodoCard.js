4561e9b077d739a0e4782067d7f436c98d7d9be5
HartMason <hartmason85@gmail.com>
2022-07-23 10:26:20 -0500

    imported css into Todo Card, added styling to remove button

--- src/TodoCard.js
+++ src/TodoCard.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React from "react";
+import TodoCard from './TodoCard.css'
 
-function ToDoCard(props){
-  return(
-    <li>{props.title}
-      <button className="removeButton" onClick={ () => { props.clickToRemove(props.index) }}>Remove</button>
-    </li>
-    
-  )
+function ToDoCard(props) {
+  return (
+    <div className="todoContainer">
+      <li className="list">{props.title}</li>
+      <button
+        className="removeButton"
+        onClick={() => {
+          props.clickToRemove(props.index);
+        }}
+      >
+        Remove
+      </button>
+    </div>
+  );
 }
 
-export default ToDoCard
+export default ToDoCard;
